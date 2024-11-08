36b5e777dffc58608d7860184b04b6ed20a5e6f0
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-07 17:12:48 -0700

    tumbleweed jumps left and right off of walls

--- src/tumbleweed.gd
+++ src/tumbleweed.gd
@@ -2,12 +2,23 @@ extends CharacterBody2D
 const _GRAVITY: float = 300.0
 const _SPEED: int = 50
 
+var is_wall_climbing: bool = false
+var horizontal: Vector2 = Vector2(-_SPEED, 0)
+
 func _ready() -> void:
-	velocity = Vector2.LEFT * _SPEED
+	velocity = horizontal
 	set_process_mode(Node.PROCESS_MODE_DISABLED)
 
 func _physics_process(delta: float) -> void:
-	if is_on_wall():
-		velocity.x = get_wall_normal().x * _SPEED
-	velocity.y = 0.0 if is_on_floor() else velocity.y + (_GRAVITY * delta)
+	print(velocity)
+	if !is_wall_climbing:
+		velocity.y = 0.0 if is_on_floor() else velocity.y + (_GRAVITY * delta)
+		if is_on_wall():
+			is_wall_climbing = true
+			velocity = Vector2(0, -_SPEED)
+	elif is_wall_climbing:
+		if !is_on_wall():
+			is_wall_climbing = false
+			horizontal = -horizontal
+			velocity = horizontal
 	move_and_slide()
