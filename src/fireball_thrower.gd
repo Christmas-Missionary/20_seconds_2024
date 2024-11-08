5ace767644ce61f54ae19bb7d6826d1857125e9f
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-08 17:38:53 +0000

    fireballs hurt player

--- src/fireball_thrower.gd
+++ src/fireball_thrower.gd
@@ -1,1 +1,12 @@
 extends RigidBody2D
+
+const _FIREBALL: PackedScene = preload("res://src/fireball.tscn")
+const _RAD_DIFF: float = PI / 4 * 3
+
+@onready var timer: = $Timer as Timer
+
+func _emit_fireball() -> void:
+	timer.start(randf_range(0.7, 1.3))
+	var ball: = _FIREBALL.instantiate() as Fireball
+	add_child(ball)
+	ball.spawn(Vector2.UP.rotated(randf_range(-_RAD_DIFF, _RAD_DIFF)))
