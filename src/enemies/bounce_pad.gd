162439e82ade731e1a1048325521298831a43e04
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-08 17:20:04 -0700

    player and tumbleweed can bounce on pad

--- src/enemies/bounce_pad.gd
+++ src/enemies/bounce_pad.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
 func _bounce_thing(body: Node) -> void:
+	const fling_vel: Vector2 = Vector2(0, -500)
 	if body is RigidBody2D:
-		(body as RigidBody2D).linear_velocity = Vector2(0, -500)
+		(body as RigidBody2D).linear_velocity += fling_vel
+	elif body is CharacterBody2D:
+		(body as CharacterBody2D).velocity += fling_vel
+		(body as CharacterBody2D).move_and_slide()
