8dbb276e26d51e48d84b68182d5e9a038bb8d590
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-09 16:49:24 -0700

    variation on fort one

---
+++ src/enemies.gd
@@ -0,0 +1,8 @@
+extends Object
+class_name Enemies
+const TUMBLEWEED: = preload("res://src/enemies/tumbleweed.tscn")
+const CACTUS: = preload("res://src/enemies/cactus.tscn")
+const FIREBALL_THROWER: = preload("res://src/enemies/fireball_thrower.tscn")
+const FIREBALL: = preload("res://src/enemies/fireball.tscn")
+const DUST_DEVIL: = preload("res://src/enemies/dust_devil.tscn")
+const BOUNCE_PAD: = preload("res://src/enemies/bounce_pad.tscn")
