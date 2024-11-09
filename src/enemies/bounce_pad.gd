88df12e60f2b04f6f9334bc9eabcfa71c8e0dd86
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-08 17:55:11 -0700

    bounce pad can bounce on others

--- src/enemies/bounce_pad.gd
+++ src/enemies/bounce_pad.gd
@@ -1,9 +1,13 @@
-extends Area2D
+extends RigidBody2D
 
 func _bounce_thing(body: Node) -> void:
 	const fling_vel: float = -400
+	if body == self:
+		return
 	if body is RigidBody2D:
-		(body as RigidBody2D).linear_velocity.y = fling_vel
+		var rigid: = body as RigidBody2D
+		rigid.linear_velocity.y = fling_vel if (rigid.position.y <= position.y) else -fling_vel
 	elif body is CharacterBody2D:
-		(body as CharacterBody2D).velocity.y = fling_vel
-		(body as CharacterBody2D).move_and_slide()
+		var chara: = body as CharacterBody2D
+		chara.velocity.y = fling_vel if (chara.position.y <= position.y) else -fling_vel
+		chara.move_and_slide()
