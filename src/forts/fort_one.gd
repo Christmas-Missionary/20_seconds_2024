ef480f6bcbde00a3e22fe54ce8507c461f0d3875
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-11 15:33:08 +0000

    changed fort_one

--- src/forts/fort_one.gd
+++ src/forts/fort_one.gd
@@ -1,7 +1,6 @@
 extends Fort
 
 func _ready() -> void:
-	_add(ENEMIES.CACTUS).position = (Vector2(90, -86) if randi() % 2 else Vector2(170, -126))
-	_add(ENEMIES.TUMBLEWEED).position = (Vector2(150, -30) if randi() % 2 else Vector2(270, -30))
-	if !(randi() % 3):
-		_add(ENEMIES.CACTUS if randi() % 2 else ENEMIES.BOUNCE_PAD).position = Vector2(50, -62)
+	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(50, -104)
+	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(210, -163)
+	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(231, -75)
