b23dfd20fa1e1d724cb1c498138065d2aa88c178
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-11 15:33:25 +0000

    made fireball thrower throw in any direction

--- src/enemies/fireball_thrower.gd
+++ src/enemies/fireball_thrower.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
 const _FIREBALL: PackedScene = preload("res://src/enemies/fireball.tscn")
-const _RAD_DIFF: float = PI / 4 * 3
 
 @onready var timer: = $Timer as Timer
 
@@ -9,4 +8,4 @@ func _emit_fireball() -> void:
 	timer.start(randf_range(0.7, 1.3))
 	var ball: = _FIREBALL.instantiate() as Fireball
 	add_sibling(ball)
-	ball.spawn(position, Vector2.UP.rotated(randf_range(-_RAD_DIFF, _RAD_DIFF)))
+	ball.spawn(position, Vector2.UP.rotated(randf_range(0, TAU)))
