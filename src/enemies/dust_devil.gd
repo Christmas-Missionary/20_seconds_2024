103ace3d72a62e9c89a12a12a17a5bf53427e4a3
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-14 18:53:31 -0700

    Changed to cpu particles for preprocess

--- src/enemies/dust_devil.gd
+++ src/enemies/dust_devil.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _physics_process(delta: float) -> void:
 	position += Vector2(-60, 0) * delta
-	player.position.x += (-60.0 if player.position.x > position.x else 60.0) * delta
+	player.position.x += (60.0 if player.position.x > position.x else -60.0) * delta
