a55629963c8f2626c43c238f12816b06e98b5eb3
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-14 19:00:17 -0700

    added dust devil to fort 1

--- src/forts/fort_one.gd
+++ src/forts/fort_one.gd
@@ -4,3 +4,5 @@ func _ready() -> void:
 	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(50, -104)
 	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(210, -163)
 	_add(ENEMIES.FIREBALL_THROWER if randi() % 2 else ENEMIES.CACTUS).position = Vector2(231, -75)
+	if !(randi() % 12):
+		_add(ENEMIES.DUST_DEVIL).position = Vector2(440, -25)
