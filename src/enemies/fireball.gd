4156df5bcd7969c669ce4a11f7ec2da966a66b34
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-14 22:00:04 -0700

    added rotations

--- src/enemies/fireball.gd
+++ src/enemies/fireball.gd
@@ -2,14 +2,17 @@ extends Area2D
 class_name Fireball
 
 var _velocity: Vector2 = Vector2.ZERO
+var _rotation_speed: float = 0.0
 
 func spawn(pos: Vector2, direction: Vector2) -> void:
 	position = pos
 	_velocity = direction * 5
+	_rotation_speed = randf_range(-0.5, 0.5)
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += (5 * delta)
 	position += _velocity
+	rotate(_rotation_speed)
 
 func _check_body(body: Node2D) -> void:
 	if body is Player:
