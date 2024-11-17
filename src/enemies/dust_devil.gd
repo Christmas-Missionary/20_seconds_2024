657b18fdf58ac06f53d8dd5f116d19d1744dc0b3
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-17 18:00:12 +0000

    dust devil can pull player both ways

--- src/enemies/dust_devil.gd
+++ src/enemies/dust_devil.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _physics_process(delta: float) -> void:
 	position += Vector2(-60, 0) * delta
-	_player.position.x += (60.0 if _player.position.x > position.x else -60.0) * delta
+	_player.position.x += 60.0 * signf(global_position.x - _player.position.x) * delta
