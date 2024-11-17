ffe033892decc06718b47c0af3b5fc58abf8e307
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-17 01:06:51 +0000

    automatic respawn feature

--- src/main/ui/play_button.gd
+++ src/main/ui/play_button.gd
@@ -2,3 +2,8 @@ extends Button
 
 func _ready() -> void:
 	pressed.connect(get_tree().set_pause.bind(false))
+	if GameStats.is_respawning_automatically:
+		pressed.emit()
+	else:
+		await get_tree().process_frame
+		get_tree().set_pause(true)
