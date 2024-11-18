56e24086c3c4a5d8ea3bde4e24b26140b665db05
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-18 02:25:20 +0000

    _set_block func

--- src/forts/fort_two.gd
+++ src/forts/fort_two.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	_add(ENEMIES.TUMBLEWEED).position = Vector2(randf_range(190, 230), -30)
 	_add(ENEMIES.TUMBLEWEED if randi() % 2 else ENEMIES.BOUNCE_PAD).position = Vector2(randf_range(130, 190), -143)
 	if (randi() % 2):
-		set_cell(Vector2i(8, -6), -1, Vector2i.ZERO)
+		_set_block(Vector2i(8, -6), false)
 		_add(ENEMIES.FIREBALL_THROWER).position = Vector2(170, -110)
 	var code: int = randi() % 3
 	if (code >= 1):
