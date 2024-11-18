69d20754dab28b48cd8947b4988496832a33430b
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-17 18:06:23 -0700

    music plays when paused and then stops if game timer is under 3 seconds

--- src/main/ui/play_button.gd
+++ src/main/ui/play_button.gd
@@ -6,3 +6,9 @@ func _ready() -> void:
 		pressed.emit()
 	else:
 		get_tree().set_pause(true)
+
+@onready var _game_timer: = $/root/Main/CanvasLayer/UI/Labels/TimeLeft/GameTimer as Timer
+
+func _check_game_time() -> void:
+	if _game_timer.time_left < 3.0:
+		MusicPlayer.set_stream_paused(true)
