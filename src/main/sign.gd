f2add24dd2beb6123b260210d876595a0cb96fea
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-12-08 20:25:54 +0000

    secret sign is more noticable

--- src/main/sign.gd
+++ src/main/sign.gd
@@ -3,3 +3,12 @@ extends Sprite2D
 func _ready() -> void:
 	if GameStats.last_score >= 50 and !(randi() % 5):
 		texture = preload("res://assets/images/secret_sign.png")
+	else:
+		set_physics_process(false)
+
+
+var _rate: float = -0.01
+func _physics_process(_delta) -> void:
+	modulate.g += _rate
+	if modulate.g <= 0.0 or modulate.g >= 1.0:
+		_rate = -_rate
