38ddabe36780651762998d6aa57a891e9906b7f4
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-12-09 00:36:52 +0000

    secret sign more to right so player doesn't miss it

--- src/main/sign.gd
+++ src/main/sign.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 func _ready() -> void:
 	if GameStats.last_score >= 50 and !(randi() % 5):
 		texture = preload("res://assets/images/secret_sign.png")
+		position.x = 500
 	else:
 		set_physics_process(false)
 
