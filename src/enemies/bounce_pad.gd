f484ce236169877a112cf198f418f71820df6c0a
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-12-09 00:36:34 +0000

    bounce pads are silent if on a cactus

--- src/enemies/bounce_pad.gd
+++ src/enemies/bounce_pad.gd
@@ -2,15 +2,20 @@ extends RigidBody2D
 
 @onready var _audio: = $AudioPlayer as AudioStreamPlayer2D
 
+var _has_just_started: bool = true
+
 func _bounce_thing(body: Node) -> void:
 	const _FLING_VEL: float = -400
 	if body == self:
 		return
 	if body is RigidBody2D:
-		_audio.play()
+		if !_has_just_started:
+			_audio.play()
+		_has_just_started = false
 		var rigid: = body as RigidBody2D
 		rigid.linear_velocity.y = _FLING_VEL if (rigid.position.y <= position.y) else -_FLING_VEL
 	elif body is CharacterBody2D:
+		_has_just_started = false
 		_audio.play()
 		var chara: = body as CharacterBody2D
 		if body is Player:
